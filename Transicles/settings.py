d1910239bdfbc3efd8d1d388882e379b4616ce58
Usman <bad199614@gmail.com>
2017-09-21 10:41:24 +0500

    Changed Title of urlshortener and shorty and turn debug false

--- Transicles/settings.py
+++ Transicles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8*#z3djn8zit!jizf*bvw*!o0=2o2ha1#vruu=r%91chz+#*58'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','transicles.pythonanywhere.com']
 
